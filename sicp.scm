2355f59f7ecdf2922df831bd7577f845f18f16e8
Karthik Chikmagalur <karthikchikmagalur@gmail.com>
2014-09-04 23:43:35 +0530

    Added Exercise 1.19.

--- sicp.scm
+++ sicp.scm
@@ -135,3 +135,67 @@
                         p
                         q
                         (- count 1)))))
+
+;;--------------------------------
+;; Exercise 1.20. The process that a procedure generates is of course
+;; dependent on the rules used by the interpreter. As an example,
+;; consider the iterative gcd procedure given above. Suppose we were
+;; to interpret this procedure using normal-order evaluation, as
+;; discussed in section 1.1.5. (The normal-order-evaluation rule for
+;; if is described in exercise 1.5.) Using the substitution method
+;; (for normal order), illustrate the process generated in evaluating
+;; (gcd 206 40) and indicate the remainder operations that are
+;; actually performed. How many remainder operations are actually
+;; performed in the normal-order evaluation of (gcd 206 40)? In the
+;; applicative-order evaluation?
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+;;                                     |   a |  b | a  | (= b 0) |
+;;  Normal order: [r := remainder]     |-----+----+----+---------|
+;; (gcd 206 40)                      ; | 206 | 40 |  0 |       0 | 
+;; (gcd 40 (r 206 40))               ; |  40 |  6 |  0 |       1 |
+;; (gcd (r 206 40)                   ; |     |    |    |         |
+;;      (r 40 (r 206 40)))           ; |   6 |  4 |  0 |       2 |
+;; (gcd (r 40 (r 206 40))            ; |     |    |    |         |
+;;      (r (r 206 40)                ; |     |    |    |         |
+;;         (r 40 (r 206 40))))       ; |   4 |  2 |  0 |       4 |
+;; (gcd (r (r 206 40)                ; |     |    |    |         |
+;;         (r 40 (r 206 40)))        ; |     |    |    |         |
+;;      (r (r 40 (r 206 40))         ; |     |    |    |         |
+;;         (r (r 206 40)             ; |     |    |    |         |
+;;            (r 40 (r 206 40)))))   ; |   2 |  0 |  4 |       7 |
+;;                                     |-----+----+----+---------|
+;;                                     |     |    |  4 |      14 |
+;;                                     |-----+----+----+---------|
+;; 
+;; The first two columns are the values of a & b (evaluated by me, not
+;; scheme.) The third column is the number of r (remainder) calls in
+;; the if statement's evaluation of 'a'. The last column is the number
+;; of r calls in the evaluation of (= b 0). The sum of the numbers in
+;; columns 3 & 4 is the total number of r calls. (+ 4 14): 18
+
+;;; Applicative order:
+;;  (gcd 206 40)
+;;; (gcd 206 40)
+;;; (gcd 40 (remainder 206 40))             ; 1
+;;; (gcd 40 6)
+;;; (gcd 6 (remainder 40 6))                ; 1
+;;; (gcd 6 4)
+;;; (gcd 4 (remainder 6 4))                 ; 1
+;;; (gcd 4 2)                               
+;;; (gcd 2 (remainder 4 2))                 ; 1
+;;; (gcd 2 0)                               ; Done: 4 remainder ops
+
+
+;;--------------------------------
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b)
+  (= (remainder b a) 0))
