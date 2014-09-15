4c1dfdd70089417bbdca58795b81fb887800b2b7
karthink <karthikchikmagalur@gmail.com>
2014-09-15 17:35:19 +0530

    Up to Ex. 2.6

--- sicp-2.1.scm
+++ sicp-2.1.scm
@@ -1,6 +1,15 @@
 ;;----------
 ;;PRIMITIVES
 ;;----------
+(define (fast-expt b n)
+  (define (even? n)
+    (= (remainder n 2) 0 ))
+  (define (fast-expt-iter b n a)
+    (cond ((= n 0) a)
+          ((even? n) (fast-expt-iter (square b) (/ n 2) a))
+          (else (fast-expt-iter b (- n 1) (* a b)))))
+  (fast-expt-iter b n 1))
+
 (define (square n) (* n n))
 (define (average a b) (/ (+ a b) 2))
 
@@ -127,6 +136,118 @@
 (define (first-side r) (make-segment (car (car r)) (cdr r)))
 (define (secnd-side r) (make-segment (cdr (car r)) (cdr r)))
 
+;;------------
+;;EXERCISE 2.4
+;;------------
 
+;;; Here is an alternative procedural representation of pairs. For
+;;; this representation, verify that (car (cons x y)) yields x for any
+;;; objects x and y. 
+
+(define (my-cons x y)
+  (lambda (m) (m x y)))
+
+(define (my-car z)
+  (z (lambda (p q) p)))
+
+;;; Hint: To verify that this works, make use of the substitution
+;;; model of section 1-1-5.
+
+;;; Verify first:
+(my-car (my-cons 0 1))                  ;0
+
+;;; my-cons returns a function that accepts a function and applies it
+;;; to x and y
+
+;;; my-car (when-called) applies my-cons to a function that takes two
+;;; arguments and returns the first one.
+
+;;; Substitution model:
+(my-car (my-cons 0 1))
+(my-car (lambda (m) (m x y)))
+((lambda (m) (m x y)) (lambda (p q) p))
+((lambda (p q) p) x y)
+x
+
+;;;  What is the corresponding definition of cdr?
+(define  (my-cdr z)
+  (z (lambda (p q) q)))
+
+(my-cdr (my-cons 0 1))                  ;1
+
+;;------------
+;;EXERCISE 2.5
+;;------------
+
+;;; Show that we can represent pairs of nonnegative integers using
+;;; only numbers and arithmetic operations if we represent the pair a
+;;; and b as the integer that is the product 2^a3^b. Give the
+;;; corresponding definitions of the procedures cons, car, and cdr.
+
+;;; Definitions of cons, car and cdr
+
+(define (econs a b)
+  (* (fast-expt 2 a) (fast-expt 3 b)))
+
+(define (divides-how-many-times z k times)
+  (if (not (= (remainder z k) 0))
+      times
+      (divides-how-many-times (/ z k) k (+ times 1))))
+
+(define (ecar z)
+  (divides-how-many-times z 2 0))
+
+(define (ecdr z)
+  (divides-how-many-times z 3 0))
+
+(ecar (econs 5 4))                      ;5
+(ecdr (econs 5 4))                      ;4
+
+;;; We need to show that the mapping a,b -> 2^a3^b is one-one.
+
+;;; Suppose there exist two pairs of nonnegative integers a,b and p,q
+;;; for which 2^a3^b = 2^p3^q. 2^(a-p) 3^(b-q) = 1. Since 2 and 3 are
+;;; (relatively) prime, the LHS represents a prime factorization of 1,
+;;; which is 1 itself. So 2^(a-p) = 3^(b-q) = 1, or a,b = p,q.
+
+;;------------
+;;EXERCISE 2.6
+;;------------
+
+;;; In case representing pairs as procedures wasn't mind-boggling
+;;; enough, consider that, in a language that can manipulate
+;;; procedures, we can get by without numbers (at least insofar as
+;;; nonnegative integers are concerned) by implementing 0 and the
+;;; operation of adding 1 as 
+
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f)
+    (lambda (x) (f ((n f) x)))))
+
+;;; This representation is known as Church numerals , after its
+;;; inventor, Alonzo Church, the logician who invented the lambda calculus.
+
+;;; Define one and two directly (not in terms of zero and add-1).
+;;; (Hint: Use substitution to evaluate (add-1 zero)). Give a direct
+;;; definition of the addition procedure + (not in terms of repeated
+;;; application of add-1).
+
+(define one
+  (lambda (f)
+    (lambda (x) (f x))))                     ;basically f
+
+(define two
+  (lambda (f)
+    (lambda (x) (f (f x)))))                 ;f(f())
+
+(define (church+ n1 n2)
+  (lambda (f)
+    (lambda (x) ((n2 f) ((n1 f) x)))))
+
+;;; (n1 f) is a lambda that composes f with itself n1 times. ((n1 f) x) is
+;;; (f (f (f ....f x). To apply f (n1 + n2) times on x, we call (n2 f) on
+;;; the result of ((n1 f) x).
 
 
